9f0609d97dab9c4397f36640210d5e7898d4830b
Nikki K <808nikz@gmail.com>
2016-12-09 21:34:16 -1000

    first commit

---
+++ js/typingTest.js
@@ -0,0 +1,17 @@
+
+function start (callback){
+  // document.addEventListener('keyup', function(){
+  //   if(userText.value === prompt.innerHTML){
+  //     document.addEventListener();
+  //   }
+  // });
+  var userText = document.getElementById('userText').value;
+  var prompt = document.getElementById('prompt').innerHTML = x;
+
+}
+
+function displayOutput (err, wpm){
+  if(err){
+  }
+  console.log(err, wpm);
+}
