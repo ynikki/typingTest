e21dd2869493a93ca75dbc67d93028af917cae60
Nikki K <808nikz@gmail.com>
2016-12-10 14:37:46 -1000

    Editing the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,24 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
 
-gulp.task('styles', function () {
-  return gulp.src('scss/styles.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('public/css'));
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      server: "./public"
+  });
+
+  gulp.watch("scss/**/*.scss", ['sass']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
 });
 
-gulp.task('watch', function () {
-  gulp.watch('scss/**/*', ['styles']);
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("scss/styles.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("public/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve']);
